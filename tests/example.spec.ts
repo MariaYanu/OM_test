f1c6ad8cfc17abe9d13f51e815b0c5caebfd6825
MariaYanu <yanushkevichmariam@gmail.com>
2024-10-05 16:05:00 +0300

    comment added

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -3,7 +3,7 @@ import {test, expect} from '@playwright/test';
 test.only('Check header', async ({ page }) => {
   const expectedHeaderText = '99 Bottles of Beer';
 
-  await page.goto('https://www.99-bottles-of-beer.net/lyrics.html');
+  await page.goto('https://www.99-bottles-of-beer.net/lyrics.html');//comment
 
   expect(await page.locator('//h1').innerText()).toEqual(expectedHeaderText)
 });
