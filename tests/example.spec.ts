9d3cfca752096e72ad2e165bcf2729b95de965ae
MariaYanu <yanushkevichmariam@gmail.com>
2024-10-15 19:53:56 +0300

    fix config

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -1,6 +1,6 @@
 import {test, expect} from '@playwright/test';
 
-test.only('Check header', async ({ page }) => {
+test('Check header', async ({ page }) => {
   const expectedHeaderText = '99 Bottles of Beer';
 
   await page.goto('https://www.99-bottles-of-beer.net/lyrics.html');
